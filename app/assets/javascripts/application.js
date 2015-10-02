8ae61438196000323296e59f20e0eff55736a30f
kt <0614takahashi@gmail.com>
2015-10-02 11:48:32 +0900

    remove turbolinks, add link at anime_details/index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+// require turbolinks
 // bootstrap-sprockets and bootstrap should not both be included in application.js
 //= require bootstrap-sprockets
 //= require_tree .
