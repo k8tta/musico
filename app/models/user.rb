5b00465ab68c2383880c255b01544db81e74802e
kt <0614takahashi@gmail.com>
2015-10-08 18:52:36 +0900

    comment out useless models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_one :artist
-  has_many :reviews
-  has_many :musics, through: :reviews
+  # has_one :artist
+  # has_many :reviews
+  # has_many :musics, through: :reviews
 end
